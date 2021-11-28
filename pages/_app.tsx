7f78f94fa4121a8914c758c482cd31037a01206c
Fatin-Shahriare-Sium <sium1206@gmail.com>
2021-11-28 18:15:08 +0600

    alhamdulillah,able to do that

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,13 @@ import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Layout from './component/layout'
 import { Provider, Client } from 'urql'
-import useUrl from './component/useUrl'
-console.log(process.env.DEVELOPMENT);
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  let { url } = useUrl()
 
   let client = new Client({
-    url: url,
+    url: 'https://ssc-batch-server.herokuapp.com/graphql',
   })
 
   return (
--- pages/component/useUrl.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const useUrl = () => {
-
-    let url = 'https://ssc-batch-server.herokuapp.com/graphql'
-    return { url }
-}
-
-export default useUrl
